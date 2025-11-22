44acba6c5e912271e741e7a5e91d3d24c5fd937c
Ian McIntosh <ianmcintosh@Ians-MacBook-Air.local>
2025-11-22 15:38:59 -0600

    Add project links and routes for AbbyMueller, DickiesSkatePark, KaraDominguez, LonelyLaundromat, MichelleTaylor, NickBike, Vanta

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Portfolio from './Portfolio/Portfolio';
 import ReverenceAndRuin from './Projects/ReverenceAndRuin/ReverenceAndRuin';
 import Nnamdi25 from './Projects/Nnamdi/Nnamdi25';
 import VirtualLimbs from './Projects/VirtualLimbs/VirtualLimbs';
-import Polacon7 from './Projects/Polacon/Polacon7';
-import Polacon8 from './Projects/Polacon/Polacon8';
-import Polacon9 from './Projects/Polacon/Polacon9';
-import Polacon10 from './Projects/Polacon/Polacon10';
+import Polacon7 from './Projects/Polacon/Polacon7/Polacon7';
+import Polacon8 from './Projects/Polacon/Polacon8/Polacon8';
+import Polacon9 from './Projects/Polacon/Polacon9/Polacon9';
+import Polacon10 from './Projects/Polacon/Polacon10/Polacon10';
 import Xpan from './Projects/Xpan/Xpan';
 import NoNoMango from './Projects/NoNoMango/NoNoMango';
 import ItalianCarFest22 from './Projects/ItalianCarFest/ItalianCarFest22';
+import AbbyMueller from './Projects/AbbyMueller/AbbyMueller';
+import DickiesSkatePark from './Projects/DickiesSkatePark/DickiesSkatePark';
+import KaraDominguez from './Projects/KaraDominguez/KaraDominguez';
+import LonelyLaundromat from './Projects/LonelyLaundromat/LonelyLaundromat';
+import MichelleTaylor from './Projects/MichelleTaylor/MichelleTaylor';
+import NickBike from './Projects/NickBike/NickBike';
+import Vanta from './Projects/Vanta/Vanta';
 import { Routes, Route } from 'react-router-dom';
 
 function Home() {
@@ -41,6 +48,13 @@ function App() {
         <Route path="/projects/virtual-limbs" element={<VirtualLimbs />} />
         <Route path="/projects/nonomango" element={<NoNoMango />} />
         <Route path="/projects/italiancarfest22" element={<ItalianCarFest22 />} />
+        <Route path="/projects/abby-mueller" element={<AbbyMueller />} />
+        <Route path="/projects/dickies-skate-park" element={<DickiesSkatePark />} />
+        <Route path="/projects/kara-dominguez" element={<KaraDominguez />} />
+        <Route path="/projects/lonely-laundromat" element={<LonelyLaundromat />} />
+        <Route path="/projects/michelle-taylor" element={<MichelleTaylor />} />
+        <Route path="/projects/nick-bike" element={<NickBike />} />
+        <Route path="/projects/vanta" element={<Vanta />} />
         <Route path="/projects/xpan" element={<Xpan />} />
         <Route path="/info" element={<About />} />
         <Route path="/contact" element={<Contact />} />
