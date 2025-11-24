92963debdedd64e2ddaa8df5f10dd599a7912d03
Ian McIntosh <ianmcintosh@Ians-MacBook-Air.local>
2025-11-24 09:15:03 -0600

    Add theme toggle (light/dark) and CSS variables

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './Main';
+import { ThemeProvider } from './theme/ThemeContext';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
